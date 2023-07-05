b5f0379f48ef005a06cb1da7548342a6aa08797d
Tristan Bereau <tristan.bereau@gmail.com>
2023-07-05 16:06:37 +0200

    Update csv and json

--- scripts/fetch_papers.py
+++ scripts/fetch_papers.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from my_scientific_profile.papers.papers import (
     fetch_all_paper_authors,
     fetch_all_paper_infos,
@@ -8,9 +10,17 @@ paper_authors = fetch_all_paper_authors()
 print(f"fetched {len(papers)} papers and {len(paper_authors)} authors")
 
 from my_scientific_profile.database.authors import save_all_paper_authors_to_s3  # noqa
-from my_scientific_profile.database.papers import save_all_papers_to_s3  # noqa
-from my_scientific_profile.database.aws_s3 import S3_BUCKET, s3_client  # noqa
+from my_scientific_profile.database.papers import save_all_papers_to_s3, convert_papers_to_dataframe  # noqa
+from my_scientific_profile.database.aws_s3 import S3_BUCKET, S3_CLIENT  # noqa
+from to_quarto.utils import ROOT_DIR
 
-save_all_papers_to_s3(s3_client=s3_client, s3_bucket=S3_BUCKET)
-save_all_paper_authors_to_s3(s3_client=s3_client, s3_bucket=S3_BUCKET)
-print(f"saved to S3 {s3_client}")
+save_all_papers_to_s3(s3_client=S3_CLIENT, s3_bucket=S3_BUCKET)
+save_all_paper_authors_to_s3(s3_client=S3_CLIENT, s3_bucket=S3_BUCKET)
+print(f"saved to S3 {S3_CLIENT}")
+
+df = convert_papers_to_dataframe(papers)
+
+path = Path(ROOT_DIR)
+team_path = path.joinpath("data")
+df.to_json(team_path.joinpath("all_papers.json"))
+df.to_csv(team_path.joinpath("all_papers.csv"))
